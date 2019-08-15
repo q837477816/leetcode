063936d4c1a6b8ff21fa034b6d3e364b725484f0
yuyu <a837477816@qq.com>
2019-08-16 00:15:15 +0800

    代码优化，去除中间变量

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,9 +1,5 @@
 // leetcode 557
 
 export default (str) => {
-    const arr = str.split(' ')
-    const result = arr.map(item => {
-        return item.split('').reverse().join('')
-    })
-    return result.join(' ')
+    return str.split(' ').map(item => item.split('').reverse().join('')).join(' ')
 }
