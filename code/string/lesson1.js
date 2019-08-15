12b44ba1975b23b0eef4a7a97bcbc564bfa89645
yuyu <a837477816@qq.com>
2019-08-16 00:24:20 +0800

    添加多种解法

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,5 +1,25 @@
 // leetcode 557
 
+// 解法1
+// export default (str) => {
+//     const arr = str.split(' ')
+//     const result = arr.map(item => {
+//         return item.split('').reverse().join('')
+//     })
+//     return result.join(' ')
+// }
+
+//解法二 （去除中间变量）
+// export default (str) => {
+//     return str.split(' ').map(item => item.split('').reverse().join('')).join(' ')
+// }
+
+// 解法三 （split方法支持传入正则）
+// export default (str) => {
+//     return str.split(/\s/g).map(item => item.split('').reverse().join('')).join(' ')
+// }
+
+// 解法四 （使用正则匹配出所有的单词)
 export default (str) => {
-    return str.split(' ').map(item => item.split('').reverse().join('')).join(' ')
+    return str.match(/[\w']+/g).map(item => item.split('').reverse().join('')).join(' ')
 }
